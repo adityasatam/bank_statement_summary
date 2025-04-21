870a9f2df0599bbdde43c0b8d866f6094184d6da
adityasatam <70567909+adityasatam@users.noreply.github.com>
2025-04-21 19:20:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,107 @@
+# input: icici bank xls format bank statement
+# set the required values: file_path, file_name, year, month, amount_greater_than
+# output: Monthly Summary of Withdrawals, Deposits and Significant Withdrawals/Deposits, Quaterly Summary of Withdrawals
+# 02-04-2025 Aditya Satam: Monthly Summary of Withdrawals, Deposits and Significant Withdrawals/Deposits
+# 20-04-2025 Aditya Satam: Quaterly Summary of Withdrawals
+
+import pandas as pd
+
+# Data reading, cleaning and EDA
+def data_cleaning_EDA(file_path, file_name):
+    # read xls file into python dataframe
+    df = pd.read_excel(file_path+file_name, engine="xlrd")
+
+    # rename xls columns
+    df.columns = ["DummyColumn", "SNo", "ValueDate", "TransationDate", 
+                  "ChequeNo", "Remark", "Withdrawal", "Deposit", "Balance"]
+
+    # filter top description till Balance column has data
+    first_valid_row = df['Balance'].first_valid_index()
+    fdf = df.loc[first_valid_row+1:] # +1 to filter out Balalnce (INR) header
+    fdf =fdf[fdf['Balance'].notna()] # filter out Nan records
+
+    # create new month, year columns using Transation date column
+    fdf['TransationDate'] = pd.to_datetime(fdf['TransationDate'], format='%d/%m/%Y')
+    fdf['Year'] = fdf['TransationDate'].dt.year.astype(int) # Extract Year
+    fdf['Month'] = fdf['TransationDate'].dt.month.astype(int) # Extract Month (Numeric)
+    fdf['MonthName'] = fdf['TransationDate'].dt.strftime('%b')  # Extract Full Month Name
+    return fdf
+
+# Monthly Summary of Withdrawals and Deposits
+def summary_dr_cr(fdf):
+    # month number, name mapping
+    month_dict = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7:
+                      'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+    Current_Balance = fdf['Balance'].iloc[-1]
+    df_dr_cr = fdf.groupby(['Month',
+                                                'Year'])[['Withdrawal',
+                                                          'Deposit']].sum().sort_values(by=['Year',
+                                                                                            'Month'], 
+                                                                                            ascending=False).reset_index()
+    
+    # Adding Savings, Balance MonthEnd
+    df_dr_cr['Savings'] = df_dr_cr['Deposit'] - df_dr_cr['Withdrawal']
+    df_dr_cr['Balance_MonthEnd'] = Current_Balance - df_dr_cr['Savings'].cumsum().shift(fill_value=0)
+    df_dr_cr['Month'] = df_dr_cr['Month'].map(month_dict)
+    print("\n>>>>>> Monthly Summary of Withdrawals and Deposits\n")
+    print(df_dr_cr)
+    return df_dr_cr
+
+# Monthly Summary of Significant Withdrawals/Deposits
+def summary_dr_cr_indiv(fdf, year, month, amount_greater_than):
+    df_dr = fdf[(fdf['Withdrawal'] > amount_greater_than) & 
+                            (fdf['Month'] == month) & 
+                            (fdf['Year'] == year)][['MonthName', 'Remark',
+                                                            'Withdrawal']].rename(columns={'Withdrawal': 'Amount'})
+    df_dr['Type'] = 'Withdrawal'
+    df_cr = fdf[(fdf['Deposit'] > amount_greater_than) & 
+                            (fdf['Month'] == month) & 
+                            (fdf['Year'] == year)][['MonthName', 'Remark',
+                                                            'Deposit']].rename(columns={'Deposit': 'Amount'})
+    df_cr['Type'] = 'Deposit'
+    df_union = pd.concat([df_dr, df_cr], ignore_index=True)
+    print("\n>>>>>> Monthly Summary of Significant Withdrawals/Deposits\n")
+    print(df_union)
+
+# Quaterly Summary of Withdrawals
+def summary_dr_qtr(df_dr_cr):
+    df_qtr = pd.DataFrame(columns=['Year', 'Quarter', 'Period', 'Actual_Period', 'Withdrawal'])
+    qtr_dict = {"Q1": ["Jan", "Feb", "Mar"], "Q2": ["Apr", "May", "Jun"], 
+                    "Q3": ["Jul", "Aug", "Sep"], "Q4": ["Oct", "Nov", "Dec"]}
+    unique_year_list = df_dr_cr['Year'].unique().tolist()
+    for y in unique_year_list:
+        for key, value in qtr_dict.items():
+            wt_sum = 0
+            mon_list = []
+            for mon in value:
+                df_dr_spent = df_dr_cr[(df_dr_cr['Month'] == mon) &
+                                                    (df_dr_cr['Year'] == y)]['Withdrawal'].sum()
+                if df_dr_spent:
+                    wt_sum += df_dr_spent
+                    mon_list.append(mon)
+            if mon_list:
+                new_row = {'Year': y, 'Quarter': key, 'Period': qtr_dict[key], 'Actual_Period': mon_list, 'Withdrawal': wt_sum}
+                if not df_qtr.empty:
+                    df_qtr = pd.concat([df_qtr, pd.DataFrame([new_row])], ignore_index=True)
+                else:
+                    df_qtr = pd.DataFrame([new_row])
+    print("\n>>>>>> Quarterly Summary of Withdrawals Spent\n")
+    print(df_qtr)
+
+def main(file_path, file_name, year, month, amount_greater_than):
+    df = data_cleaning_EDA(file_path, file_name)
+
+    summary_dr_cr_indiv(df, year, month, amount_greater_than)
+    res_df = summary_dr_cr(df)
+    summary_dr_qtr(res_df)
+
+
+# # set file path and name
+# file_path=r"C:/Users/sasuk/"
+# file_name="OpTransactionHistoryTpr02-04-2025 (1).xls"
+
+# # set the filter for 2nd report on Top Withdrawal and Deposit
+# year=2024
+# month=11
+# amount_greater_than=5000
+main(file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1).xls", year=2024, month=11, amount_greater_than=5000)
