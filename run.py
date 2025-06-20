32c74b0aad66aa70630b7c4e7c2fe82211c9d399
adityasatam <70567909+adityasatam@users.noreply.github.com>
2025-06-20 22:57:35 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import requests
 
+# pre-requisite:
+# 1) go to this url: "https://github.com/adityasatam/bank_statement_summary/blob/main/main.py", click on the Raw option, copy the latest url below.
+# 2) copy this run.py in local VS Code and execute.
+
 url = "https://raw.githubusercontent.com/adityasatam/bank_statement_summary/refs/heads/main/main.py?token=GHSAT0AAAAAADEQJLXYAXX4DA5ZH264TCD42BTLJNQ"
 response = requests.get(url)
 
 if response.status_code == 200:
     exec(response.text)
     #print(response.text)
-    #main(bank_name='icici', file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1).xls", year=2025, month=1, amount_greater_than=5000)
+    main(bank_name='icici', file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1).xls", year=2025, month=1, amount_greater_than=5000)
     #main(bank_name='hdfc', file_path=r"C:/Users/sasuk/", file_name="Acct_Statement_XX7897_20062025.xls", year=2025, month=5, amount_greater_than=5000)
 else:
     print("Failed to execute the github code")
