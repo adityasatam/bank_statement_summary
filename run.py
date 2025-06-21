e16fecfffae7678f04bae31b5952d06785b63c5e
adityasatam <70567909+adityasatam@users.noreply.github.com>
2025-06-21 10:21:08 +0530

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ response = requests.get(url)
 if response.status_code == 200:
     exec(response.text)
     #print(response.text)
-    main(bank_name='icici', file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1).xls", year=2025, month=1, amount_greater_than=5000)
-    #main(bank_name='hdfc', file_path=r"C:/Users/sasuk/", file_name="Acct_Statement_XX7897_20062025.xls", year=2025, month=5, amount_greater_than=5000)
+    main(bank_name='icici', file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1).xls", year=2025, month=[1,2], amount_greater_than=5000, remark_match='VIN')
+    #main(bank_name='hdfc', file_path=r"C:/Users/sasuk/", file_name="Acct_Statement_XX7897_20062025.xls", year=2025, month=[4], amount_greater_than=5000, remark_match='')
 else:
     print("Failed to execute the github code")
