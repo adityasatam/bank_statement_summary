e31d6ea7238521559816bcae5200908ea7b3e335
adityasatam <70567909+adityasatam@users.noreply.github.com>
2025-06-21 11:41:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import requests
 
-# 1) keep your bank statement in local folder.
+# 1) keep your bank statement in xls format in local folder.
 # 2) copy this run.py in your local VS Code.
 # 4) modify only line 13 with bank statement file name, file path and other parameters.
 # 5) execute run.py in your local VS Code.
@@ -11,7 +11,7 @@ response = requests.get(url)
 if response.status_code == 200:
     exec(response.text)
     #print(response.text)
-    main(bank_name='icici', file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1).xls", year=2025, month=[1,2,3,4], amount_greater_than=1000, remark_match='VIN/')
-    #main(bank_name='hdfc', file_path=r"C:/Users/sasuk/", file_name="Acct_Statement_XX7897_20062025.xls", year=2025, month=[4,5,6], amount_greater_than=1000, remark_match='POS ')
+    main(bank_name='icici', file_path=r"C:/Users/sasuk/", file_name="OpTransactionHistoryTpr02-04-2025 (1)", year=2025, month=[1,2,3,4], amount_greater_than=1000, remark_match='VIN/')
+    #main(bank_name='hdfc', file_path=r"C:/Users/sasuk/", file_name="Acct_Statement_XX7897_20062025", year=2025, month=[4,5,6], amount_greater_than=1000, remark_match='POS ')
 else:
     print("Failed to execute the github code")
